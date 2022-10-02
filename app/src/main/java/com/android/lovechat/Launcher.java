2707200025c5a49e418a241f62657d720fa54086
NikitaNakahara <nikita.maer14@gmail.com>
2022-10-03 04:51:56 +0500

    added SignIn activity

--- app/src/main/java/com/android/lovechat/Launcher.java
+++ app/src/main/java/com/android/lovechat/Launcher.java
@@ -1,17 +1,53 @@
 package com.android.lovechat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Launcher extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        setTheme(R.style.Theme_LoveChat);
+        String config = "";
+        try {
+            config = readConfigFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        setContentView(R.layout.main);
+        if (config == null) {
+            startActivity(new Intent(this, SignIn.class));
+
+            finish();
+        }
 
         super.onCreate(savedInstanceState);
+    }
+
+    String readConfigFile() throws IOException {
+        FileReader reader;
+        try {
+            reader = new FileReader(getFilesDir() + "/main.conf");
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+
+        StringBuilder config = new StringBuilder();
+
+        int confChar;
+        while ((confChar = reader.read()) != -1) {
+            config.append(confChar);
+        }
+
+        return config.toString();
     }
 }
---
+++ app/src/main/java/com/android/lovechat/SignIn.java
@@ -0,0 +1,17 @@
+package com.android.lovechat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SignIn extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.Theme_LoveChat);
+
+        setContentView(R.layout.main);
+
+        super.onCreate(savedInstanceState);
+    }
+}
